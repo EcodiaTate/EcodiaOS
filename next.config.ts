a3b91ee848b357f58bf97f12583ff2f6890da7bf
Tate <tate@ecodia.au>
2025-10-22 18:24:20 +1000

    things

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  transpilePackages: ["three", "@react-three/fiber", "@react-three/drei"],
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(glsl|vs|fs|vert|frag)$/i,
+      exclude: /node_modules/,
+      use: [
+        { loader: "raw-loader" },       // makes file contents a JS string
+        { loader: "glslify-loader" },   // applies glslify transforms/includes
+      ],
+    });
+    return config;
+  },
+  experimental: { typedRoutes: true },
 };
 
 export default nextConfig;
